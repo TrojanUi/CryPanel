eaa29262906b4343cc12a8d3661e69638388fc5e
Trojan <trojandev@mail.ru>
2022-11-24 00:16:04 +0400

    Backend #0.1
    Создание модели юзера(ВКЛЮЧАЕТ В СЕБЯ ПРАВА)
    права либо забаненный, либо норм тип
    так же права админа, в случае если подобное понадобится например для тп аккаунты создавать.
    Созданна модель domains - название домена, вариант шаблона, owner - человек к которому прикрученном, и количество установленных программ на этом шаблоне(для статистики)

--- crypan/main/admin.py
+++ crypan/main/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import Custom,domains
 # Register your models here.
+admin.site.register(domains)
+
+@admin.register(Custom)
+class CustomUserAdmin(UserAdmin):
+    model = Custom
+    
+    list_display = ['username','user_rights']
+    add_fieldsets=(
+        *UserAdmin.add_fieldsets,
+        (
+            'Custom fields',
+            {
+                'fields':(
+                  'user_rights', 
+                )
+            }
+        )
+    )
+    
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'Custom fields',
+            {
+                'fields':(
+                    'user_rights',
+        )
+            }
+        )
+    )
--- crypan/main/models.py
+++ crypan/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+rights =[
+    ('no_rights','no_rights'), #blocked user
+    ('standart_user','standart_user'),# standart user buy/no buy sites
+    ('administrator','administrator'),# variant - if site sell(no actual variant)
+]
+shablons = [
+    ('steamfish','steamfish')
+]
+class Custom(AbstractUser):
+    user_rights = models.CharField(max_length=30,choices=rights,default='standart_user')
+    
+class domains(models.Model):
+    name_domain = models.TextField('Name of domain(search function)',blank=False)
+    variant = models.CharField('Choice of shablons',max_length=30,choices=shablons)
+    owner = models.ForeignKey(Custom,on_delete=models.CASCADE)
+    downloads = models.IntegerField('number of downloaded programs',default=0)
