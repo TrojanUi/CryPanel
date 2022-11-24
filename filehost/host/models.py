c7212c0d139eec8ea0e4bd86b930887a9ebc62d3
Trojan <trojandev@mail.ru>
2022-11-24 18:21:54 +0400

    ГОТОВЫЙ ФАЙЛООБМЕНИК БЕЗ ДИЗАЙНА

--- filehost/host/models.py
+++ filehost/host/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class element(models.Model):
+    file = models.FileField('Файл на сайте')
+    def __str__(self):
+        return 'жески файл'
+        
--- filehost/host/views.py
+++ filehost/host/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from host.models import element
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        h = request.FILES['fileob']
+        a = element(file = h)
+        a.save()
+        host = request.META['HTTP_HOST']
+        urlka = f'{host}/media/{h}'
+        context={
+            'urlka':urlka
+        }
+        return render(request,'main/index.html',context)
     return render(request,'main/index.html')
+
+        
